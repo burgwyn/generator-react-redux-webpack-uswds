74acf5460167c9b31e7737033ca26f1c43639b0d
Nat Burgwyn <burgwyn@gmail.com>
2016-06-10 07:04:26 -0400

    Initial deployment

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,11 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
+
+  initializing: function () {
+    this.pkg = require('../../package.json');
+  },
+
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
@@ -23,11 +28,69 @@ module.exports = yeoman.Base.extend({
     }.bind(this));
   },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  writing: {
+
+    packageJSON: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+    },
+
+    babel: function () {
+      this.fs.copy(
+        this.templatePath('babelrc'),
+        this.destinationPath('.babelrc')
+      );
+    },
+
+    git: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore'));
+
+      this.fs.copy(
+        this.templatePath('gitattributes'),
+        this.destinationPath('.gitattributes'));
+    },
+
+    eslint: function () {
+      this.fs.copy(
+        this.templatePath('eslintrc'),
+        this.destinationPath('.eslintrc')
+      );
+    },
+
+    jsconfig: function () {
+      this.fs.copy(
+        this.templatePath('jsconfig.json'),
+        this.destinationPath('jsconfig.json')
+      );
+    },
+
+    webpack: function () {
+      this.fs.copy(
+        this.templatePath('webpack.config.js'),
+        this.destinationPath('webpack.config.js')
+      );
+    },
+
+    scripts: function () {
+      this.fs.copy(
+        this.templatePath('index.jsx'),
+        this.destinationPath('src/index.jsx')
+      );
+    },
+
+    html: function () {
+      this.fs.copy(
+        this.templatePath('index.html'),
+        this.destinationPath('index.html'),
+        {
+          appname: this.appname
+        }
+      );
+    }
   },
 
   install: function () {
